2bda9b3fb01dd9e30994f4c010c3cbd7dd7e8226
Bartłomiej Połomski <bartek.polomski@gmail.com>
2018-12-01 12:30:11 +0100

    master Inital project

---
+++ src/main/java/spring/rabbitmq/delayedexchange/DelayedExchangeApplication.java
@@ -0,0 +1,12 @@
+package spring.rabbitmq.delayedexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelayedExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DelayedExchangeApplication.class, args);
+    }
+}
